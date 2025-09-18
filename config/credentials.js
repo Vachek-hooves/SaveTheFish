88e0a59aa3ac70068febc5ba59f8e58ea095e060
Vachek-hooves <vachek0602@gmail.com>
2025-09-18 17:03:22 +0300

    tested

---
+++ config/credentials.js
@@ -0,0 +1,29 @@
+// appFlyer initSdk options
+
+export const OPTIONS = {
+  devKey: 'g8aJrZujPTN9QWkpo5EWL4',
+  // appId: '',
+  onInstallConversionDataListener: true,
+  onDeepLinkListener: true,
+  timeToWaitForATTUserAuthorization: 10,
+  manualStart: true,
+};
+
+// secret FB key - e0b722d6b164e544618e2e2724a01e6f
+
+// FB app cred
+export const FB_APP_ID = '1328883542171964';
+export const FB_CLIENT_TOKEN = '2339721a0255165ec2f6a3a8338f0f04';
+
+// Compaign
+// https://frost-cedar-bay.space/KDrMbfqi
+export const INITIAL_URL = `https://frost-cedar-bay.space/`;
+export const URL_IDENTIFAIRE = `KDrMbfqi`;
+
+// one signal key ex 3553 Holland Tulip Quiz
+export const ONE_SIGNAL_ID = '7a48c97b-fe32-4991-98a6-9929f5b607fe';
+
+export const USER_AGENT =
+  '"Mozilla/5.0 (Linux; Android 13; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"';
+
+// s2s token - a6ea945d-08ac-4d51-b96b-275378ed15d4
